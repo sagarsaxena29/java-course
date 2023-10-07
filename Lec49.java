9363bf2cbedeb089a426b675872db4af49bb2ed9
sagarsaxena29 <sagarsaxena6395@gamil.com>
2023-10-07 23:18:57 +0530

    Changes

---
+++ Lec49.java
@@ -0,0 +1,26 @@
+//Dynamic Method Dispatch 
+
+class phone{
+    public void call(){
+        System.out.println("Calling.......");
+    }
+    public void On(){
+        System.out.println("Turning on phone.......");
+    }
+}
+class smartphone extends phone{
+    public void Music(){
+        System.out.println("select a song");
+    }
+    public void On(){
+        System.out.println("Turning on smartphone......");
+    }
+}
+public class Lec49 {
+    public static void main(String[] args) {
+    phone obj = new smartphone();
+        obj.call();
+        obj.On();
+}
+
+}
