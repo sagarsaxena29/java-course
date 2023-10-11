dd3d3434bcb651b1d188623c268e1b17ca2dddab
sagarsaxena29 <sagarsaxena6395@gamil.com>
2023-10-11 15:28:47 +0530

    changes

---
+++ Lec57.java
@@ -0,0 +1,47 @@
+interface Mycamera{
+    void Recordvideo();
+    void TakeSnap();
+    default void Record4K(){
+        System.out.println("Record in 4K...");
+    }
+}
+interface MyWifi{
+    String[] getNeworks();
+    void connnectToNetwork(String network);
+}
+class MYCellPhone {
+    void callNumber(int phoneNumber){
+        System.out.println("callling..."+phoneNumber);
+    }
+    void pickCall(){}
+}
+class MySmartphone extends MYCellPhone implements Mycamera , MyWifi{
+    public void Recordvideo(){
+        System.out.println("Recording.....start");
+    }
+    public void TakeSnap(){
+        System.out.println("Taking snap...");
+    }
+    public  String[] getNeworks(){
+        System.out.println("networks are.... ");
+        String [] networkList = {"Airtel 4G" ," Jio 5G "};
+        return networkList;
+    }
+    public void connnectToNetwork(String network){
+        System.out.println("Connecting to network....."+network);
+    }
+}
+
+public class Lec57 {
+    public static void main(String[] args) {
+        MySmartphone vivo = new MySmartphone();
+        String [] ar = vivo.getNeworks();
+        for (String item : ar) {
+    
+            System.out.println(item);
+        }
+        vivo.connnectToNetwork("BSNL");
+       //System.out.println(vivo.getNeworks()); 
+    }
+    
+}
